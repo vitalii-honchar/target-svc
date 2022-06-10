9c8ff286335c84b3dc2d87470717e54990ac7a23
Vitalii Honchar <weaxme@gmail.com>
2022-06-10 18:44:54 +0200

    Added tests for get list of targets

--- src/app.test.mjs
+++ src/app.test.mjs
@@ -5,11 +5,7 @@ describe('test app', () => {
 
     it('should create a target', async () => {
         const app = await build()
-        const response = await createTarget(app, new Target(null, 'My target'))
-
-        expect(response.statusCode).toEqual(200)
-
-        const target = JSON.parse(response.body)
+        const target = await createTarget(app, new Target(null, 'My target'))
 
         expect(target.description).toEqual('My target')
         expect(target.id).not.toBeNull()
@@ -18,11 +14,8 @@ describe('test app', () => {
     it('should create a target with id greater than previous', async () => {
         const app = await build()
 
-        const response1 = await createTarget(app, new Target(null, 'test'))
-        const response2 = await createTarget(app, new Target(null, 'test'))
-
-        const target1 = JSON.parse(response1.body)
-        const target2 = JSON.parse(response2.body)
+        const target1 = await createTarget(app, new Target(null, 'test'))
+        const target2 = await createTarget(app, new Target(null, 'test'))
         expect(target2.id).toEqual(target1.id + 1)
     })
 
@@ -36,12 +29,29 @@ describe('test app', () => {
         const targets = JSON.parse(response.body)
         expect(targets.length).toEqual(0)
     })
+
+    it('should receive a list with targets', async () => {
+        const app = await build()
+
+        const target1 = await createTarget(app, new Target(null, 'test 1'))
+        const target2 = await createTarget(app, new Target(null, 'test 2'))
+        const response = await app.inject({
+            method: 'GET',
+            url: '/target'
+        })
+
+        const targets = JSON.parse(response.body)
+        expect(targets.length).toEqual(2)
+        expect(targets[0].id).toEqual(target1.id)
+        expect(targets[1].id).toEqual(target2.id)
+    })
 })
 
 async function createTarget(app, target) {
-    return app.inject({
+    const response = await app.inject({
         method: 'POST',
         url: '/target',
         payload: target
     })
+    return JSON.parse(response.body)
 }
