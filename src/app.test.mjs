d5d1daf03df73f6ce6f7e3fc814441cf539394ac
Vitalii Honchar <weaxme@gmail.com>
2022-06-10 18:16:16 +0200

    Refactored tests

--- src/app.test.mjs
+++ src/app.test.mjs
@@ -1,17 +1,11 @@
 import build from './app.mjs'
+import {Target} from "./domain/target.mjs";
 
 describe('test app', () => {
 
     it('should create a target', async () => {
         const app = await build()
-
-        const response = await app.inject({
-            method: 'POST',
-            url: '/target',
-            payload: {
-                description: 'My target'
-            }
-        })
+        const response = await createTarget(app, new Target(null, 'My target'))
 
         expect(response.statusCode).toEqual(200)
 
@@ -24,23 +18,19 @@ describe('test app', () => {
     it('should create a target with id greater than previous', async () => {
         const app = await build()
 
-        const response1 = await app.inject({
-            method: 'POST',
-            url: '/target',
-            payload: {
-                description: 'My target'
-            }
-        })
-        const response2 = await app.inject({
-            method: 'POST',
-            url: '/target',
-            payload: {
-                description: 'My target'
-            }
-        })
+        const response1 = await createTarget(app, new Target(null, 'test'))
+        const response2 = await createTarget(app, new Target(null, 'test'))
 
         const target1 = JSON.parse(response1.body)
         const target2 = JSON.parse(response2.body)
         expect(target2.id).toEqual(target1.id + 1)
     })
 })
+
+async function createTarget(app, target) {
+    return app.inject({
+        method: 'POST',
+        url: '/target',
+        payload: target
+    })
+}
